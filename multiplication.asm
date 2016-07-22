12ac18d75e67ebf85f7c7a56952789c6ce7a1fab
Jaromir Obr <jaromir.obr@gmail.com>
2016-07-22 22:26:35 +0200

    Added printing of full multiplication expression

--- multiplication.asm
+++ multiplication.asm
@@ -1,12 +1,31 @@
     *= $3400
     .include math.asm
-    multiply 209, 123 ; multiply two numbers
-    hexToDec ; convert the product to string of decimal digits
+    .include screen.asm
+    .include keyboard.asm
 
-; wait for key press
-CH = 764
-key     lda #$FF
-        cmp CH
-        beq key
-        sta CH
-        rts
+num1 = 56
+num2 = 229
+    ldx #<num1
+    ldy #>num1
+    makeDigitsPrintable multiplier1
+
+    ldx #<num2
+    ldy #>num2
+    makeDigitsPrintable multiplier2
+    
+    multiply num1, num2 ; multiply two numbers
+    makeDigitsPrintable product
+    strlen = endofstring - multiplier1 
+    printStr multiplier1, strlen ; print in format "num1*num2=product"
+    keypress ; stops program unless you press key
+    rts
+
+decimal .ds decDigitCnt  
+multiplier1
+        .ds decDigitCnt
+    .sbyte "*"
+multiplier2
+        .ds decDigitCnt
+    .sbyte "="
+product .ds decDigitCnt
+endofstring
