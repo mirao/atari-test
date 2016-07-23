92cc2926f501d427efcbe70531ce225ae08d59dc
Jaromir Obr <jaromir.obr@gmail.com>
2016-07-23 02:05:03 +0200

    Playing with MADS

--- hello.asm
+++ hello.asm
@@ -1,3 +1,12 @@
-    *= $2400
-    lda #1
-    sta 752
+    ORG $3400
+    icl "hellomc.mac"
+    majne string2-string
+    majne $20
+    ldy #0
+    mva #$11 ($58),y
+    jmp *
+
+string
+    dta c"1Retez"
+string2
+    dta d"1Retez"
